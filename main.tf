5cdeef1d0d5d702d0702b9e79844d67331249281
mu <muhammed.abdelghafar@outlook.com>
2024-10-01 17:27:39 +0000

    initialize

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "depi-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "web_vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # Adjust as necessary
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "web_subnet"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  vpc_id      = aws_vpc.main.id
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere (consider restricting this for production)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  # Security group allowing SSH and HTTP access
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  # User data script to install Apache
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
+              EOF
+
+  tags = {
+    Name = "Web-Server"
+  }
+}
+
+
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "us-east-1a"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-005fc0f236362e99f"
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = "~/.ssh/github-actions-key.pub"
+}
