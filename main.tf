15399031d27915a5e71c09f8a9cecb604d969f55
mu <muhammed.abdelghafar@outlook.com>
2024-10-02 08:47:25 +0000

    correct aws region var name

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
+  region     = var.aws-region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region" {
+variable "aws-region" {
   type    = string
   default = "us-east-1"
 }
